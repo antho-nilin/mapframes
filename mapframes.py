e0728777da3a429eb9ba68a10b0e3630d38796a2
unknown <anthony.lin4321@gmail.com>
2023-07-02 04:39:42 +1000

    Added randomness for more natural looking transitions

--- mapframes.py
+++ mapframes.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 import sys
+import random
 
 img = Image.open("start.png").convert("RGB")
 img_array = np.array(img)
@@ -23,7 +24,7 @@ visited = {}
 def test(pixelmap, endmap, coords, startingpixels, attacking_color, visited):
     if (coords in visited):
         return
-    if (pixelmap[coords] != attacking_color and endmap[coords] == attacking_color):
+    elif (pixelmap[coords] != attacking_color and endmap[coords] == attacking_color):
         startingpixels.append(coords)
         visited[coords] = True
         print(coords)
@@ -34,10 +35,13 @@ while len(startingpixels) != 0:
     startingpixels = []
     for pixel in curpixels:
         pixelmap[pixel] = attacking_color
-        test(pixelmap, endmap, (pixel[0], pixel[1]+1), startingpixels, attacking_color, visited)
-        test(pixelmap, endmap, (pixel[0], pixel[1]-1), startingpixels, attacking_color, visited)
-        test(pixelmap, endmap, (pixel[0]+1, pixel[1]), startingpixels, attacking_color, visited)
-        test(pixelmap, endmap, (pixel[0]-1, pixel[1]), startingpixels, attacking_color, visited)
+        if random.random() < 0.5:
+            test(pixelmap, endmap, (pixel[0], pixel[1]+1), startingpixels, attacking_color, visited)
+            test(pixelmap, endmap, (pixel[0], pixel[1]-1), startingpixels, attacking_color, visited)
+            test(pixelmap, endmap, (pixel[0]+1, pixel[1]), startingpixels, attacking_color, visited)
+            test(pixelmap, endmap, (pixel[0]-1, pixel[1]), startingpixels, attacking_color, visited)
+        else:
+            startingpixels.append(pixel)
     img.save(f"output{count}.png")
     count+=1
 
